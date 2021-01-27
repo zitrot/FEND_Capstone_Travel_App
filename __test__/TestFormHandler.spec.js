d346cd789a460b229192af02e1352e2278c71664
zitrot <mortiza3@eafit.edu.co>
2021-01-26 20:37:23 -0500

    correction of jest test

--- __test__/TestFormHandler.spec.js
+++ __test__/TestFormHandler.spec.js
@@ -1,4 +1,4 @@
-import { handleSubmit } from "../src/client/js/formHandler"
+import { handleSubmit } from "../src/client/js/HandleController"
 
 // The describe() function takes two arguments - a string description, and a test suite as a callback function.  
 // A test suite may contain one or more related tests    
